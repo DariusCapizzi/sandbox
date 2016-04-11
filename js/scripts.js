00416e2312ab42985fda5164b0c7acbc3d10b48d
Josh Bertsche <jjjbertsche@gmail.com>
2016-04-11 12:40:39 -0700

    checks their relative location

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,11 @@ function Piece(pieceName, img, currentPos, relativePos, relatedPieces) {
 
 Piece.prototype.check = function() {
   //this.relatedPieces.forEach(function(related) {
-    console.log(this.relatedPieces[0].currentPos[0] - this.currentPos[0]);
-    console.log(this.relatedPieces[0].relativePos[0] - this.relativePos[0]);
+    // console.log(this.relatedPieces[0].currentPos[0]);
+    // console.log(this.currentPos[0]);
+    //   console.log(this.relatedPieces[0].relativePos[0]);
+    //   console.log(this.relativePos[0]);
+    //   console.log("")
     //check left coordinate
     if (this.relatedPieces[0].currentPos[0] - this.currentPos[0] === this.relatedPieces[0].relativePos[0] - this.relativePos[0]) {
       alert("Yeah!");
@@ -18,8 +21,8 @@ Piece.prototype.check = function() {
 }
 
 
-var cat1 = new Piece(cat1, "cat1.jpg", [10,50], [0,0]);
-var cat2 = new Piece(cat2, "cat2.jpg", [270,0], [500,0]);
+var cat1 = new Piece("cat1", "cat1.jpg", [10,50], [0,0]);
+var cat2 = new Piece("cat2", "cat2.jpg", [270,0], [500,0]);
 cat1.relatedPieces = [cat2];
 cat2.relatedPieces = [cat1];
 var cats = [cat1, cat2];
@@ -35,8 +38,10 @@ $(function() {
   $(".piece").draggable({
     drag: function() {
       cats.forEach(function(cat) {
-        var left = $("#" + cat.pieceName).css("left");
-        var top =  $("#" + cat.pieceName).css("top");
+        console.log(cat.pieceName);
+        var left = parseInt($("#" + cat.pieceName).css("left"));
+        var top = parseInt($("#" + cat.pieceName).css("top"));
+        console.log([left, top]);
         cat.currentPos = [left, top];
         cat.check();
       });

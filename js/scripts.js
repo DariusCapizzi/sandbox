97230a1943ef60f2f85e2f5608c6eb7e16871e27
Josh Bertsche <jjjbertsche@gmail.com>
2016-04-15 10:24:41 -0700

    add forward, backward, and delete buttons

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ function Pic(element) {
   this.blur = 0;
   this.sepia = 0;
   this.invert = 0;
+  this.zIndex = 0;
 }
 
 var selectedPics = [];
@@ -94,6 +95,32 @@ $(function () {
     });
   });
 
+  $("#join").click(function () {
+    joinPics();
+  });
+
+  $("#delete").click(function () {
+    selectedPics.forEach(function (pic) {
+      delete canvas[pic.id];
+      $("#" + pic.id).remove();
+      updateSelectedPics();
+    });
+  });
+
+  $("#forward").click(function () {
+    selectedPics.forEach(function (pic) {
+      pic.zIndex += 1;
+      drawPic(pic);
+    });
+  });
+
+  $("#backward").click(function () {
+    selectedPics.forEach(function (pic) {
+      pic.zIndex -= 1;
+      drawPic(pic);
+    });
+  });
+
   function addPic(element) {
     //add newPic object and add image to document
     var newPic = new Pic(element);
@@ -101,14 +128,22 @@ $(function () {
     drawPic(newPic);
   }
 
-  function drawPic(newPic) {
+  function drawPic(newPic, parentDiv) {
+    if (!parentDiv) {
+      parentDiv = "body";
+    }
+    console.log(parentDiv)
     $("#" + newPic.id).remove();
 
     // make and set html attributes based on newPic
-    $(".main").append(newPic.element);
+    $(parentDiv).append(newPic.element);
     $(".pic").last().attr("id", newPic.id);
     $("#" + newPic.id).css("left", newPic.position[0]);
     $("#" + newPic.id).css("top", newPic.position[1]);
+    //draw chidlren
+    for (var pic in newPic.children) {
+      drawPic(pic, "#" + newPic.id)
+    }
     //set selected
     if (newPic.selected) {
       $("#" + newPic.id).addClass("selected");
@@ -123,6 +158,8 @@ $(function () {
     // sets blur, sepia, invert
     $("#" + newPic.id).css("filter", "blur(" + newPic.blur + "px) sepia(" + newPic.sepia + ") invert(" + newPic.invert + ")");
     $("#" + newPic.id).css("-webkit-filter", "blur(" + newPic.blur + "px) sepia(" + newPic.sepia + ") invert(" + newPic.invert + ")");
+    // sets z-index
+    $("#" + newPic.id).css("z-index", newPic.zIndex);
 
     // sets element to be draggable
     $("#" + newPic.id).draggable({
@@ -140,80 +177,81 @@ $(function () {
   }
 
   function drawAll() {
-    $(".main").empty();
+    $("body").empty();
     for (var pic in canvas) {
       console.log(pic);
       drawPic(pic);
     }
   }
 
-  function joinPics() {
-    console.log(selectedPics);
-    var selectedIds = [];
-    var selectedLeft = [];
-    var selectedTop = [];
-
-    selectedPics.forEach(function (pic) {
-      selectedIds.push(pic.id);
-      selectedLeft.push(pic.position[0]);
-      selectedTop.push(pic.position[1]);
-    });
-    selectedIds = "#" + selectedIds.join(", #");
-
-    // determine coordinates, width, and height of wrapping div --TODO: DOES NOT ALWAYS WORK RIGHT
-    var sortedLeft = selectedLeft.sort(function (a, b) {
-      return a - b;
-    });
-    var sortedTop = selectedTop.sort(function (a, b) {
-      return a - b;
-    });
-    var rightMostPic = selectedPics[selectedLeft.indexOf(sortedLeft[sortedLeft.length - 1])];
-    var bottomMostPic = selectedPics[selectedTop.indexOf(sortedTop[sortedTop.length - 1])];
-
-    var newLeft = sortedLeft[0];
-    var newTop = sortedTop[0];
-    var newWidth = rightMostPic.position[0] - newLeft + rightMostPic.width;
-    var newHeight = bottomMostPic.position[1] - newTop + bottomMostPic.height;
-
-    //create new Pic for group
-    var newPic = new Pic("");
-    canvas[newPic.id] = newPic;
-    newPic.position = [newLeft, newTop];
-    newPic.width = newWidth;
-    newPic.height = newHeight;
-    newPic.childen = selectedPics;
-
-    // wrap in div and set coordinates, width, and height
-    $(selectedIds).wrapAll("<div class='group pic' id='" + newPic.id + "' />");
-    $(".group").last().css("left", newPic.position[0]);
-    $(".group").last().css("top", newPic.position[1]);
-    $(".group").last().css("width", newPic.width);
-    $(".group").last().css("height", newPic.height);
-
-    selectedPics.forEach(function (pic) {
-      pic.position[0] -= newLeft;
-      pic.position[1] -= newTop;
-      $("#" + pic.id).css("left", pic.position[0]);
-      $("#" + pic.id).css("top", pic.position[1]);
-      pic.selected = false;
-      $("#" + pic.id).removeClass("selected");
-      $("#" + pic.id).off();
-      $("#" + pic.id).draggable("disable");
-    });
-
-    $(".group").last().click(function () {
-      $(this).toggleClass("selected");
-      canvas[$(this).attr("id")].selected = !canvas[$(this).attr("id")].selected;
-      updateSelectedPics();
-    });
-
-    $(".group").last().draggable({
-      drag: function () {
-        newPic.position = [parseInt($(this).css("left")), parseInt($(this).css("top"))];
-      }
-    });
-    updateSelectedPics();
-  }
+  // function joinPics() {
+  //   console.log("join");
+  //   var selectedIds = [];
+  //   var selectedLeft = [];
+  //   var selectedTop = [];
+  //
+  //   selectedPics.forEach(function (pic) {
+  //     selectedIds.push(pic.id);
+  //     selectedLeft.push(pic.position[0]);
+  //     selectedTop.push(pic.position[1]);
+  //   });
+  //   selectedIds = "#" + selectedIds.join(", #");
+  //
+  //   // determine coordinates, width, and height of wrapping div --TODO: DOES NOT ALWAYS WORK RIGHT
+  //   var sortedLeft = selectedLeft.sort(function (a, b) {
+  //     return a - b;
+  //   });
+  //   var sortedTop = selectedTop.sort(function (a, b) {
+  //     return a - b;
+  //   });
+  //   var rightMostPic = selectedPics[selectedLeft.indexOf(sortedLeft[sortedLeft.length - 1])];
+  //   var bottomMostPic = selectedPics[selectedTop.indexOf(sortedTop[sortedTop.length - 1])];
+  //
+  //   var newLeft = sortedLeft[0];
+  //   var newTop = sortedTop[0];
+  //   var newWidth = rightMostPic.position[0] - newLeft + rightMostPic.width;
+  //   var newHeight = bottomMostPic.position[1] - newTop + bottomMostPic.height;
+  //
+  //   //create new Pic for group
+  //   var newPic = new Pic("");
+  //   canvas[newPic.id] = newPic;
+  //   newPic.position = [newLeft, newTop];
+  //   newPic.width = newWidth;
+  //   newPic.height = newHeight;
+  //   newPic.childen = selectedPics;
+  //   newPic.
+  //
+  //   // wrap in div and set coordinates, width, and height
+  //   $(selectedIds).wrapAll("<div class='group pic' id='" + newPic.id + "' />");
+  //   $(".group").last().css("left", newPic.position[0]);
+  //   $(".group").last().css("top", newPic.position[1]);
+  //   $(".group").last().css("width", newPic.width);
+  //   $(".group").last().css("height", newPic.height);
+  //
+  //   selectedPics.forEach(function (pic) {
+  //     pic.position[0] -= newLeft;
+  //     pic.position[1] -= newTop;
+  //     $("#" + pic.id).css("left", pic.position[0]);
+  //     $("#" + pic.id).css("top", pic.position[1]);
+  //     pic.selected = false;
+  //     $("#" + pic.id).removeClass("selected");
+  //     $("#" + pic.id).off();
+  //     $("#" + pic.id).draggable("disable");
+  //   });
+  //
+  //   $(".group").last().click(function () {
+  //     $(this).toggleClass("selected");
+  //     canvas[$(this).attr("id")].selected = !canvas[$(this).attr("id")].selected;
+  //     updateSelectedPics();
+  //   });
+  //
+  //   $(".group").last().draggable({
+  //     drag: function () {
+  //       newPic.position = [parseInt($(this).css("left")), parseInt($(this).css("top"))];
+  //     }
+  //   });
+  //   updateSelectedPics();
+  // }
 
   function updateSelectedPics() {
     selectedPics = [];
